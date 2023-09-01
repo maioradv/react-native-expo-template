3e49194c96fe56e8d551d8c67f2103c5bdd9287f
Sabatino Carotenuto <sabatinocarotenuto@maioradv.com>
2023-09-01 16:19:11 +0200

    prova

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,9 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { MMKV } from 'react-native-mmkv' 
+
+const storage = new MMKV();
+
+
 
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
+      <TextInput onChangeText={(text:any) => storage.set('name',text)} />
+      <Text style={styles.title}>{storage.getString('name')}</Text>
       <View style={styles.separator}/>
     </View>
   );
